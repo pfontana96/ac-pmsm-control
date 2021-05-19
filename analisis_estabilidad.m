b794f3099947cdfd9f0c903aa1cba37827b9148a
mTorres95 <miactl@gmail.com>
2021-05-19 22:51:54 +0200

    Agrega observabilidad y controlabilidad

--- analisis_estabilidad.m
+++ analisis_estabilidad.m
@@ -11,14 +11,14 @@ P_p = 3; % Pares de polos magneticos (6 polos)
 lambda_m = 0.01546; % Flujo magnetico eq de imanes [V.s/rad]
 L_q = 5.8e-3; % Inductancia estator eje en cuadratura [H]
 R_s = 1.02;
-% Funcion de transferencia
-H = tf([L_q  R_s],[L_q*J_eq  b_eq*L_q+J_eq*R_s  b_eq*R_s+(3*P_p^2*lambda_m^2)/2  0])
-
-% Calculo de polos de la funcion
-p = pole(H)
-
-% Calculo de zeros de la funcion
-z = zero(H)
+% % Funcion de transferencia
+% H = tf([L_q  R_s],[L_q*J_eq  b_eq*L_q+J_eq*R_s  b_eq*R_s+(3*P_p^2*lambda_m^2)/2  0])
+% 
+% % Calculo de polos de la funcion
+% p = pole(H)
+% 
+% % Calculo de zeros de la funcion
+% z = zero(H)
 
 % Gráfico de los polos y ceros
 %pzmap(H)
@@ -32,15 +32,15 @@ db_l = 0.0630; % [N.m.s/rad]
 
 J_eq_max = J_m + (J_l+dJ_l)/r^2;
 b_eq_max = b_m + (b_l+db_l)/r^2;
-H_max = tf([L_q  R_s],[L_q*J_eq_max  b_eq_max*L_q+J_eq_max*R_s  b_eq_max*R_s+(3*P_p^2*lambda_m^2)/2  0]);
-p_max = pole(H_max)
-z_max = zero(H_max)
+% H_max = tf([L_q  R_s],[L_q*J_eq_max  b_eq_max*L_q+J_eq_max*R_s  b_eq_max*R_s+(3*P_p^2*lambda_m^2)/2  0]);
+% p_max = pole(H_max)
+% z_max = zero(H_max)
 
 J_eq_min = J_m + (J_l-dJ_l)/r^2;
 b_eq_min = b_m + (b_l-db_l)/r^2;
-H_min = tf([L_q  R_s],[L_q*J_eq_min  b_eq_min*L_q+J_eq_min*R_s  b_eq_min*R_s+(3*P_p^2*lambda_m^2)/2  0]);
-p_min = pole(H_min)
-z_min = zero(H_min)
+% H_min = tf([L_q  R_s],[L_q*J_eq_min  b_eq_min*L_q+J_eq_min*R_s  b_eq_min*R_s+(3*P_p^2*lambda_m^2)/2  0]);
+% p_min = pole(H_min)
+% z_min = zero(H_min)
 
 %pzmap(H,H_max,H_min)
 %grid on
@@ -48,19 +48,52 @@ z_min = zero(H_min)
 %-------------------------------------------------
 % Teniendo en cuenta las variaciones de R_s con T°
 %-------------------------------------------------
+% 
+% for R_s = 1.02:0.05:1.32
+%     H = tf([L_q  R_s],[L_q*J_eq  b_eq*L_q+J_eq*R_s  b_eq*R_s+(3*P_p^2*lambda_m^2)/2  0]);
+%     H_max = tf([L_q  R_s],[L_q*J_eq_max  b_eq_max*L_q+J_eq_max*R_s  b_eq_max*R_s+(3*P_p^2*lambda_m^2)/2  0]);
+%     H_min = tf([L_q  R_s],[L_q*J_eq_min  b_eq_min*L_q+J_eq_min*R_s  b_eq_min*R_s+(3*P_p^2*lambda_m^2)/2  0]);
+%     pzmap(H,'g',H_max,'r',H_min,'b')
+%     hold on
+% end
+% grid on
 
-for R_s = 1.02:0.05:1.32
-    H = tf([L_q  R_s],[L_q*J_eq  b_eq*L_q+J_eq*R_s  b_eq*R_s+(3*P_p^2*lambda_m^2)/2  0]);
-    H_max = tf([L_q  R_s],[L_q*J_eq_max  b_eq_max*L_q+J_eq_max*R_s  b_eq_max*R_s+(3*P_p^2*lambda_m^2)/2  0]);
-    H_min = tf([L_q  R_s],[L_q*J_eq_min  b_eq_min*L_q+J_eq_min*R_s  b_eq_min*R_s+(3*P_p^2*lambda_m^2)/2  0]);
-    pzmap(H,'g',H_max,'r',H_min,'b')
-    hold on
-end
-grid on
-
-%-------------------------------------------------
+%=================================================
 % Calculo de frecuencia natural y amortiguamiento
-%-------------------------------------------------
+%=================================================
+disp("Frecuencia natural y amortiguamiento a 40°C")
 R_s = 1.02; % A 40°C
-w_n = ((b_eq*R_s + (3*P_p^2*lambda_m^2)/2)/(L_q*J_eq))^(1/2)
-ksi = (b_eq*L_q + J_eq*R_s)/(2*L_q*J_eq*w_n)
+w_n = ((b_eq*R_s + (3*P_p^2*lambda_m^2)/2)/(L_q*J_eq))^(1/2);
+ksi = (b_eq*L_q + J_eq*R_s)/(2*L_q*J_eq*w_n);
+
+% Valores minimos y máximos de los coeficientes equivalentes
+w_n_max = ((b_eq_max*R_s + (3*P_p^2*lambda_m^2)/2)/(L_q*J_eq_max))^(1/2);
+ksi_max = (b_eq_max*L_q + J_eq_max*R_s)/(2*L_q*J_eq_max*w_n);
+
+w_n_min = ((b_eq_min*R_s + (3*P_p^2*lambda_m^2)/2)/(L_q*J_eq_min))^(1/2);
+ksi_min = (b_eq_min*L_q + J_eq_min*R_s)/(2*L_q*J_eq_min*w_n);
+
+disp(".   | nominal    |    max     |    min ")
+X = sprintf('w_n | %f | %f | %f ',w_n,w_n_max,w_n_min);
+disp(X)
+X = sprintf('ksi | %f   | %f   | %f ',ksi,ksi_max,ksi_min);
+disp(X)
+
+disp("Frecuencia natural y amortiguamiento a 115°C")
+R_s = 1.32; % A 115°C
+w_n = ((b_eq*R_s + (3*P_p^2*lambda_m^2)/2)/(L_q*J_eq))^(1/2);
+ksi = (b_eq*L_q + J_eq*R_s)/(2*L_q*J_eq*w_n);
+
+% Valores minimos y máximos de los coeficientes equivalentes
+w_n_max = ((b_eq_max*R_s + (3*P_p^2*lambda_m^2)/2)/(L_q*J_eq_max))^(1/2);
+ksi_max = (b_eq_max*L_q + J_eq_max*R_s)/(2*L_q*J_eq_max*w_n);
+
+w_n_min = ((b_eq_min*R_s + (3*P_p^2*lambda_m^2)/2)/(L_q*J_eq_min))^(1/2);
+ksi_min = (b_eq_min*L_q + J_eq_min*R_s)/(2*L_q*J_eq_min*w_n);
+
+disp(".   | nominal    |    max     |    min ")
+X = sprintf('w_n | %f | %f | %f ',w_n,w_n_max,w_n_min);
+disp(X)
+X = sprintf('ksi | %f   | %f   | %f ',ksi,ksi_max,ksi_min);
+disp(X)
+
--- transferencia_lti_simplificado.m
+++ transferencia_lti_simplificado.m
@@ -1,6 +1,6 @@
 clear; clc;
 % Calculos funciones de transferencia para sistema LTI simplificado
-syms b_eq J_eq P_p lambda_m L_q R_s r s;
+syms b_eq J_eq P_p lambda_m L_q R_s r s L_d;
 A = [0 1                 0 
      0 -b_eq/J_eq        3*P_p*lambda_m/(2*J_eq)
      0 -P_p*lambda_m/L_q -R_s/L_q];
@@ -13,3 +13,49 @@ D = [0 0 0];
 sys_tf = (C/(s*eye(3)-A))*B + D;
 sys_tf
 simplify(sys_tf)
+
+
+%=================================================
+%                Observabilidad
+%=================================================
+disp("==== OBSERVABILIDAD - para theta_m")
+C =  [1 0 0]; % theta_m
+O_ = [C
+      C*A
+      C*(A*A)]
+X = sprintf('Rango de observabilidad: %d -> es observable desde la salida theta_m',rank(O_));
+disp(X)
+
+disp("==== OBSERVABILIDAD - para w_m")
+C =  [0 1 0]; % w_m
+O_ = [C
+      C*A
+      C*(A*A)]
+X = sprintf('Rango de observabilidad: %d -> NO es observable desde la salida w_m',rank(O_));
+disp(X)
+
+disp("==== OBSERVABILIDAD - para i_q")
+C =  [0 0 1]; % i_q
+O_ = [C
+      C*A
+      C*(A*A)]
+X = sprintf('Rango de observabilidad: %d -> NO es observable desde la salida i_q\n',rank(O_));
+disp(X)
+ 
+%=================================================
+%                Controlabilidad
+%=================================================
+disp("==== CONTROLABILIDAD ====")
+B_aux = B(:,1);  % La entrada T_l es una perturbacion (no se puede controlar). Usamos solo la tension v_q
+C_ = [B_aux,A*B_aux,(A*A)*B_aux]
+X = sprintf('Rango de controlabilidad: %d -> el modelo LTI SIMPLIFICADO es controlable desde v_q\n\n',rank(C_));
+disp(X)
+disp('Modelo LTI AUMENTADO -> dinamica residual del eje d, que agrega i_d que no tiene entrada')
+disp(' => sistema NO es totalmente controlable desde v_q')
+disp(' ==> se agrega una entrada al eje d')
+B_aux = [[B(:,1);0] [0; 0; 0; 1/L_d]];
+A_aux = [[A(:,1);0] [A(:,2);0] [A(:,3);0] [0;0;0;-R_s/L_d]];
+C_ = [B_aux, A_aux*B_aux, (A_aux*A_aux)*B_aux]
+X = sprintf('Rango de controlabilidad: %d -> el modelo LTI AUMENTADO es controlable desde v_q y v_d',rank(C_));
+disp(X)
+ 
